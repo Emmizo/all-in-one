153c33cda4e3a78a6bc06a92243b4a78d81d3290
Emmizo <emmizokwizera@gmail.com>
2025-02-20 17:58:11 +0200

    new changes

--- real-estate/src/App.jsx
+++ real-estate/src/App.jsx
@@ -1,12 +1,15 @@
 
 import React from "react"
-import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import About from "./components/About"
 
 function App() {
 
   return (
-    <div>
-  <Navbar/>
+    <div className="w-full overflow-hidden">
+
+      <Header/>
+      <About/>
     </div>
   )
 }
